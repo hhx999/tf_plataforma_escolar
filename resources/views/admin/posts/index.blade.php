0824b887b071366dfd8bd7847c8ffaee35c68d93
Borjas Vega Riveras <borjas.vegariveras@gmail.com>
2020-10-20 14:19:50 -0300

    TG-29 #closed Agregada la visualización de los posts en administración

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -15,4 +15,39 @@
 
 @section('content')
 
+<!-- /.box -->
+<div class="box">
+	<div class="box-header">
+		<h3 class="box-title">Data Table With Full Features</h3>
+	</div>
+		<!-- /.box-header -->
+		<div class="box-body">
+			<table id="posts-table" class="table table-bordered table-striped">
+				<thead>
+	                <tr>
+	                  <th>ID</th>
+	                  <th>Título</th>
+	                  <th>Extracto</th>
+	                  <th>Acciones</th>
+	                </tr>
+               	</thead>
+               	<tbody>
+               		@foreach($posts as $post)
+               			<tr>
+               				<td>{{ $post->id }}</td>
+               				<td>{{ $post->title }}</td>
+               				<td>{{ $post->excerpt }}</td>
+               				<td>
+               					<a href="#" class="btn btn-xs btn-info"><i class="fa fa-pencil"></i></a>
+               					<a href="#" class="btn btn-xs btn-danger"><i class="fa fa-times"></i></a>
+               				</td>
+               			</tr>
+               		@endforeach
+               	</tbody>
+            </table>
+        </div>
+       <!-- /.box-body -->
+</div>
+<!-- /.box -->
+
 @stop
