27899533fd31354b6b357b3589b1c0db75961511
Borjas Vega Riveras <borjas.vegariveras@gmail.com>
2020-11-12 03:44:58 -0300

    TG-116 #closed Creando agrupación de posts por fechas

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -22,10 +22,18 @@ class PagesController extends Controller
     }
     public function archive()
     {
+        $archive = Post::selectRaw('year(published_at) year')
+                    ->selectRaw('monthname(published_at) month')
+                    ->selectRaw('count(*) posts')
+                    ->groupBy('year','month')
+                    ->orderBy('published_at')
+                    ->get();
+
     	return view('pages.archive', [
             'docentes' => User::latest()->take(4)->get(),
             'categories' => Category::all(),
-            'posts' => Post::latest()->take(5)->get()
+            'posts' => Post::latest()->take(5)->get(),
+            'archive' => $archive
         ]);
     }
     public function contact()
